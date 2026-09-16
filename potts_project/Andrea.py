f1d4d5961d05b94030f40ff4d7101cf02d84cba6
Andrea Raaschou <andrea.raaschou@gmail.com>
2026-09-16 09:58:03 +0200

    start creating block plots, not finished

--- potts_project/Andrea.py
+++ potts_project/Andrea.py
@@ -86,10 +86,63 @@ def phase_transition_simulation(L, q, num_steps, state_type, filename):
             if step >= 50:  # Discard initial steps 
                 total_energy = calculate_total_energy(current_grid)
                 average_energy = total_energy / (L * L)  # Normalize energy per spin
-                energies_step.append(average_energy)
+                #energies_step.append(average_energy) # Dont do this here anymore, keep the full energy simulation
+                energies.append([T, average_energy])
 
-        energies.append([T, np.mean(energies_step)])
-        np.savetxt(filename, energies) # Save mean energies to a text file
+        #energies.append([T, np.mean(energies_step)]) # Skip this step
+        np.savetxt(filename, energies) # Save energies to a text file
+
+
+def calculate_mean_and_error(filename, block_sizes):
+    data = np.loadtxt(filename)
+    temperatures = data[:, 0]
+    energies = data[:, 1]
+    unique_temperatures = np.unique(temperatures)
+
+    output = []
+    for T in unique_temperatures:
+        energy_at_T = energies[temperatures == T]
+        mean_energy = np.mean(energy_at_T)
+
+        standard_errors = [] # stores the SE for each block size
+        for block_size in block_sizes: # calculate SE for different each block size
+            num_blocks = len(energy_at_T) // block_size
+            block_means = np.array([np.mean(energy_at_T[i * block_size:(i + 1) * block_size]) for i in range(num_blocks)])
+            SE = np.sqrt(np.var(block_means, ddof = 1) / num_blocks)
+            standard_errors.append(SE)
+        output.append([T, mean_energy] + standard_errors)
+    return np.array(output)
+
+"""def try_different_block_sizes(filename, block_sizes):
+    # block_sizes: array with the block sizes one wants to calculate error for
+    for i in range(num_block_sizes):
+        calculate_mean_and_error()"""
+
+# Use file with simulated phase transition data to print possible block sizes where all blocks will have the same size
+def print_possible_block_sizes(filename):
+    data = np.loadtxt(filename)
+    unique_T, counts = np.unique(data[:, 0], return_counts=True) # Get the amount of simulated energies for each temperature
+    num_energies = counts[0]
+    for n in range(1, num_energies + 1):
+        if num_energies % n == 0:
+            print(n)
+
+
+
+
+
+"""def block_error(filename, block_size):
+    energies = np.loadtxt(filename)
+    # for T in energies[0]: # For each temperature in the file
+    # calculate the mean energy (overall)
+    # divide the samples into blocks - make sure that the blocks have the same size (think about this later)
+    # return array (3 columns) with T, mean, df
+    num_blocks = len(energies) // block_size
+    block_means = np.array([np.mean(energies[i * block_size:(i + 1) * block_size]) for i in range(num_blocks)])
+    mean_energy = np.mean(block_means)
+    error = np.sqrt(np.var(block_means, ddof = 1) / num_blocks)
+    print(f"Mean energy (hot start): {mean_energy:.4f} ± {error:.4f}")
+    return mean_energy, error"""
 
 def make_time_evolution_plot(filename_hot, filename_cold):
     energies_hot = np.loadtxt(filename_hot)
@@ -120,27 +173,57 @@ def make_phase_transition_plot(filename_q2, filename_q10):
     plt.legend()
     plt.show()
 
-def block_error(filename, block_size = 100):
-    energies = np.loadtxt(filename)
-    num_blocks = len(energies) // block_size
-    block_means = np.array([np.mean(energies[i * block_size:(i + 1) * block_size]) for i in range(num_blocks)])
-    mean_energy = np.mean(block_means)
-    error = np.sqrt(np.var(block_means, ddof = 1) / num_blocks)
-    print(f"Mean energy (hot start): {mean_energy:.4f} ± {error:.4f}")
-    return mean_energy, error
+# Make phase transition plots with errorbars using different block sizes
+def make_phase_transition_plot_with_errorbars(data_q2, data_q10, block = 2):
+    # the argument block determines which block to look at
+    Tc_q2 = 1 / np.log(1 + np.sqrt(2))  # Critical temperature for q=2
+    #Tc_q10 = 1 / np.log(1 + np.sqrt(10))
+
+    #plt.plot(data_q2[:, 0], data_q2[:, 1], '.', color = 'green', label="q=2")
+    #plt.plot(data_q10[:, 0], data_q10[:, 1], '.', color = 'blue', label="q=10")
+    plt.errorbar(data_q2[:, 0], data_q2[:, 1], data[:, block], fmt = ".")
+    plt.axvline(Tc_q2, color = 'green', linestyle="--", label=f"$T_c$ (q=2)")
+    #plt.axvline(Tc_q10, color = 'blue', linestyle="--", label=f"$T_c$ (q=10)")
+    plt.title("Phase Transition for q=2 and q=10")
+    plt.xlabel("Temperature (T)")
+    plt.ylabel("Average Energy per spin (E/N)")
+    plt.legend()
+    plt.show()
+
+def produce_block_plots(data):
+    T = data[:, 0]
+    mean = data[:, 1]
+
+    for i in range(len(data[0])-2): # loop through SE for the different block sizes
+        print(i)
+
+    
+
+
    
 # Run simulation 
-#simulation(16, 2, 1.0/3.0, 500, "hot", filename="hot_start.txt")
+#simulation(16, 2, 1.0/3.0, 500, "hot", filename="hot_start.txt") # Temp = 3
 #simulation(16, 2, 1.0/3.0, 500, "cold", filename="cold_start.txt")
-#phase_transition_simulation(100, 2, 100, "cold", filename="phase_transition_q2.txt")
-#phase_transition_simulation(100, 10, 100, "cold", filename="phase_transition_q10.txt")
+#phase_transition_simulation(10, 2, 100, "cold", filename="phase_transition_q2.txt")
+#phase_transition_simulation(10, 10, 100, "cold", filename="phase_transition_q10.txt")
+
+# Run some caluclations on phase transition simulation data
+#print_possible_block_sizes("phase_transition_q10.txt")
+#output = calculate_mean_and_error("phase_transition_q10.txt", 5)
+#try_different_block_sizes("phase_transition_q10.txt", 5)
+
+data = calculate_mean_and_error("phase_transition_q10.txt", block_sizes = [5, 10])
+#for i in range(50):
+    #print(data[i])
+
+#produce_block_plots(data)
+make_phase_transition_plot_with_errorbars(data, data, block = 3)
 
 # Make plots
 #make_time_evolution_plot("hot_start.txt", "cold_start.txt")
-make_phase_transition_plot("phase_transition_q2.txt", "phase_transition_q10.txt")   
+#make_phase_transition_plot("phase_transition_q2.txt", "phase_transition_q10.txt")   
 
-# Compute error
-#block_error("hot_start.txt", block_size=50)
+
 
 
 
